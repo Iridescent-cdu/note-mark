729eb244da31b58b25f2d26e8979eb15a4403d95
iridescent <2389504513@qq.com>
2024-07-26 01:12:34 +0800

    ✨ feat(drag): 添加DraggableTopBar

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,15 @@
 import { RootLayout, Sidebar, Content } from './components/AppLayout'
+import DraggableTopBar from './components/DraggableTopBar'
 
-function App(): JSX.Element {
+const App = () => {
   return (
-    <RootLayout>
-      <Sidebar className="p-2"></Sidebar>
-      <Content className="border-1 bg-zinc-900/50 border-l-white/20"></Content>
-    </RootLayout>
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2"></Sidebar>
+        <Content className="border-1 bg-zinc-900/50 border-l-white/20"></Content>
+      </RootLayout>
+    </>
   )
 }
 
