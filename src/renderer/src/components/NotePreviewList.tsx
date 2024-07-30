e168f2cb145cd8216dde5675c23afb368601c3f9
iridescent <2389504513@qq.com>
2024-07-30 22:34:40 +0800

    ✨ feat(component): 添加NotePreviewList组件

---
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -0,0 +1,11 @@
+import { notesMock } from '@/store/mocks'
+import { ComponentProps } from 'react'
+export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
+  return (
+    <ul {...props}>
+      {notesMock.map((note) => {
+        return <li key={note.title}>{note.title}</li>
+      })}
+    </ul>
+  )
+}
