46f17536150d210f5585e799002113f0f889b1ad
iridescent <2389504513@qq.com>
2024-07-30 22:48:14 +0800

    ✨ feat(component): 添加NotePreview

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,11 +1,12 @@
 import { notesMock } from '@/store/mocks'
 import { ComponentProps } from 'react'
+import { NotePreview } from './NotePreview'
 export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
   return (
     <ul {...props}>
-      {notesMock.map((note) => {
-        return <li key={note.title}>{note.title}</li>
-      })}
+      {notesMock.map((note) => (
+        <NotePreview key={note.title} {...note} />
+      ))}
     </ul>
   )
 }
