ac0003793471fc813e39ec6759531e36890b65ae
iridescent <2389504513@qq.com>
2024-07-30 22:52:42 +0800

    ✨ feat(util): 添加formatDateFromMs

--- src/renderer/src/components/NotePreview.tsx
+++ src/renderer/src/components/NotePreview.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/renderer/utils'
+import { cn, formatDateFromMs } from '@/renderer/utils'
 import { NoteInfo } from '@/shared/models'
 import { ComponentProps } from 'react'
 
@@ -27,7 +27,9 @@ export const NotePreview = ({
       {...props}
     >
       <h3 className="mb-1 font-bold truncate">{title}</h3>
-      <span className="inline-block w-full mb-2 text-xs font-light text-left">{lastEditTime}</span>
+      <span className="inline-block w-full mb-2 text-xs font-light text-left">
+        {formatDateFromMs(lastEditTime)}
+      </span>
     </div>
   )
 }
--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge'
 export const cn = (...args: ClassValue[]): string => {
   return twMerge(clsx(args))
 }
+
+const dataFormatter = new Intl.DateTimeFormat('en-US', {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'UTC'
+})
+
+export const formatDateFromMs = (ms: number): string => {
+  return dataFormatter.format(ms)
+}
