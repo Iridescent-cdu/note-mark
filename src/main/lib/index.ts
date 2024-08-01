e5a0b174db1a499abe8ddb62d79334cc0115cd27
iridescent <2389504513@qq.com>
2024-08-01 23:08:51 +0800

    ✨ feat(context): 添加getNotes监听

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -1,6 +1,6 @@
 import { appDirectoryName, fileEncoding } from '@/shared/constants'
 import { NoteInfo } from '@/shared/models'
-import { GetNotes } from '@/shared/type'
+import { GetNotes } from '@/shared/types'
 import { ensureDir, readdir, stat } from 'fs-extra'
 import { homedir } from 'os'
 
